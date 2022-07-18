7ad739458548a003edfe8feeedb0c4a5b11aef09
swapp1990 <swapnil.sawant@arabidopsis.org>
2022-07-18 00:02:42 -0700

    update title and description

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: "Meshmill",
-  title: "Meshmill - Web3 Game Builder",
-  description: "Meshmill Landing - Nextjs/React with Tailwind CSS",
+  title: "Meshmill - The Asset Layer of metaverse",
+  description: "Technology that enables content creators to build in-game assets that traverse securely through metaverses while preserving their utility value.",
   locale: "en",
 };
